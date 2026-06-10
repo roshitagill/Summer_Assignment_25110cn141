c2fa66ca479715206bb2a36123c7c9cf4e4c57d4
unknown <roshitagill@gmail.com>
2026-06-10 20:38:28 +0530

    day 8

---
+++ day_8/30th.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i = 1;
+    int j = 1;
+    cout << "enter number of rows:";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << '\n';
+    }
+}
---
+++ day_8/32th.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i = 1;
+    int j = 1;
+
+    cout << "enter number of rows:";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << '\n';
+    }
+}
